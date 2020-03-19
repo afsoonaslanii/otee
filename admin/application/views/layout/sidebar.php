3a78f2109f800191ca7ce9d7295a9d407bd4f2a7
afsoonaslanii <aslani.a7260@gmail.com>
2020-03-19 04:28:42 +0330

    [add][admin][views][layout, shared] add layout , shared

---
+++ admin/application/views/layout/sidebar.php
@@ -0,0 +1,66 @@
+<div class="page-sidebar sidebar">
+	<div class="page-sidebar-inner slimscroll">
+		<div class="sidebar-header">
+			<div class="sidebar-profile">
+				<a href="javascript:void(0);" id="profile-menu-link">
+					<div class="sidebar-profile-image">
+						<?php
+						if ($myavatar == NULL) {
+							print'<img class="img-circle img-responsive" src="http://otee.ir/assets/images/' . $mygender . '.png" alt="' . $myfname . '">';
+						} else {
+							print '<img  src="http://otee.ir/assets/images/' . $myavatar . '" class="img-circle img-responsive"  alt="' . $myfname . '"/>';
+						}
+
+						?>
+
+					</div>
+					<div class="sidebar-profile-details">
+						<span><?php echo "$myfname"; ?> <?php echo "$mylname"; ?>
+							<br>
+							<small> او-تی | ادمین</small>
+						</span>
+					</div>
+				</a>
+			</div>
+		</div>
+
+		<ul class="menu accordion-menu">
+			<li class="<?php echo $active_sidebar_item === 'dashboard' ? 'active' : 'not-active' ?>">
+				<a href="<?php echo base_url(); ?>index.php/dashboard" class="waves-effect waves-button">
+					<span class="menu-icon glyphicon glyphicon-home"></span>
+					<p>داشبورد</p>
+				</a>
+			</li>
+			<li class="<?php echo $active_sidebar_item === 'teachers' ? 'active' : 'not-active' ?>">
+				<a href="<?php echo base_url(); ?>index.php/teachers" class="waves-effect waves-button"><span
+						class="menu-icon glyphicon glyphicon-user"></span>
+					<p>معلمان</p>
+				</a>
+			</li>
+			<li class="<?php echo $active_sidebar_item === 'students' ? 'active' : 'not-active' ?>">
+				<a href="<?php echo base_url(); ?>index.php/students" class="waves-effect waves-button">
+					<span class="menu-icon glyphicon glyphicon glyphicon-education"></span>
+					<p>دانش آموزان</p>
+				</a>
+			</li>
+			<li class="<?php echo $active_sidebar_item === 'exam' ? 'active' : 'not-active' ?>">
+				<a href="<?php echo base_url(); ?>index.php/exam" class="waves-effect waves-button">
+					<span class="menu-icon glyphicon glyphicon-book"></span>
+					<p>امتحانات</p>
+				</a>
+			</li>
+			<li class="<?php echo $active_sidebar_item === 'question' ? 'active' : 'not-active' ?>">
+				<a href="<?php echo base_url(); ?>index.php/question" class="waves-effect waves-button">
+					<span class="menu-icon glyphicon glyphicon-question-sign"></span>
+					<p>سوالات</p>
+				</a>
+			</li>
+			<li class="<?php echo $active_sidebar_item === 'results' ? 'active' : 'not-active' ?>">
+				<a href="<?php echo base_url(); ?>index.php/results" class="waves-effect waves-button">
+					<span class="menu-icon glyphicon glyphicon-certificate"></span>
+					<p>نتایج امتحان</p>
+				</a>
+			</li>
+		</ul>
+	</div>
+</div>
---
+++ admin/application/views/shared/links.php
@@ -0,0 +1,16 @@
+<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,600' rel='stylesheet' type='text/css'>
+<link href="<?php echo $url; ?>/assets/plugins/pace-master/themes/blue/pace-theme-flash.css" rel="stylesheet"/>
+<link href="<?php echo $url; ?>/assets/plugins/uniform/css/uniform.default.min.css" rel="stylesheet"/>
+<link href="<?php echo $url; ?>/assets/plugins/bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
+<link href="<?php echo $url; ?>/assets/plugins/fontawesome/css/font-awesome.css" rel="stylesheet" type="text/css"/>
+<link href="<?php echo $url; ?>/assets/plugins/line-icons/simple-line-icons.css" rel="stylesheet" type="text/css"/>
+<link href="<?php echo $url; ?>/assets/plugins/waves/waves.min.css" rel="stylesheet" type="text/css"/>
+<link href="<?php echo $url; ?>/assets/plugins/switchery/switchery.min.css" rel="stylesheet" type="text/css"/>
+<link href="<?php echo $url; ?>/assets/plugins/3d-bold-navigation/css/style.css" rel="stylesheet" type="text/css"/>
+<link href="<?php echo $url; ?>/assets/plugins/slidepushmenus/css/component.css" rel="stylesheet" type="text/css"/>
+<link href="<?php echo $url; ?>/assets/images/icon.png" rel="icon">
+<link href="<?php echo $url; ?>/assets/css/modern.min.css" rel="stylesheet" type="text/css"/>
+<link href="<?php echo $url; ?>/assets/css/themes/green.css" class="theme-color" rel="stylesheet" type="text/css"/>
+<link href="<?php echo $url; ?>/assets/css/custom.css" rel="stylesheet" type="text/css"/>
+<script src="<?php echo $url; ?>/assets/plugins/3d-bold-navigation/js/modernizr.js"></script>
+<script src="<?php echo $url; ?>/assets/plugins/offcanvasmenueffects/js/snap.svg-min.js"></script>
