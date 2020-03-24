91940c062b045f58fd6dda304ecfee7a1d0f4f53
afsoonaslanii <aslani.a7260@gmail.com>
2020-03-24 12:57:33 +0430

    [update][student][views] rtl

--- student/application/views/shared/links.php
+++ student/application/views/shared/links.php
@@ -7,6 +7,7 @@
 <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,600' rel='stylesheet' type='text/css'>
 <link href="<?php echo $url; ?>/assets/plugins/pace-master/themes/blue/pace-theme-flash.css" rel="stylesheet"/>
 <link href="<?php echo $url; ?>/assets/plugins/uniform/css/uniform.default.min.css" rel="stylesheet"/>
+<link href="<?php echo $url; ?>/assets/plugins/bootstrap/css/bootstrap-rtl.min.css" rel="stylesheet" type="text/css"/>
 <link href="<?php echo $url; ?>/assets/plugins/fontawesome/css/font-awesome.css" rel="stylesheet" type="text/css"/>
 <link href="<?php echo $url; ?>/assets/plugins/line-icons/simple-line-icons.css" rel="stylesheet" type="text/css"/>
 <link href="<?php echo $url; ?>/assets/plugins/offcanvasmenueffects/css/menu_cornerbox.css" rel="stylesheet" type="text/css"/>
@@ -18,6 +19,8 @@
 <link href="<?php echo $url; ?>/assets/css/modern.min.css" rel="stylesheet" type="text/css"/>
 <link href="<?php echo $url; ?>/assets/css/themes/green.css" class="theme-color" rel="stylesheet" type="text/css"/>
 <link href="<?php echo $url; ?>/assets/css/custom.css" rel="stylesheet" type="text/css"/>
+
+<link href="http://localhost/iQuiz/assets/css/rtl.css" rel="stylesheet" type="text/css"/>
 
 <script src="<?php echo $url; ?>/assets/plugins/3d-bold-navigation/js/modernizr.js"></script>
 <script src="<?php echo $url; ?>/assets/plugins/offcanvasmenueffects/js/snap.svg-min.js"></script>
