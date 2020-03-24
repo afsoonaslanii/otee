fc23e1e62c25ba0d5bcd5d5a7b6a5db1aed5aef4
afsoonaslanii <aslani.a7260@gmail.com>
2020-03-24 14:48:38 +0430

    [update][student, teacher][models] add user_type and phone to query

--- student/application/models/User_model.php
+++ student/application/models/User_model.php
@@ -2,39 +2,44 @@
 
 class User_model extends CI_Model
 {
-    function __construct()
-    {
-        parent::__construct();
-    }
+	function __construct()
+	{
+		parent::__construct();
+	}
 
-    function insert_user($data){
-        $this->db->insert('tbl_user', $data);
-        return true;
-    }
+	function insert_user($data)
+	{
+		$this->db->insert('tbl_user', $data);
+		return true;
+	}
 
-    function select_valid_user($username,$password){
-        $this->db->select('*');
-        $this->db->where('username',"$username");
-        $this->db->where('password',"$password");
-        $this->db->from('tbl_user');
+	function select_valid_user($username, $password)
+	{
+		$this->db->select('*');
+		$this->db->where('username', "$username");
+		$this->db->or_where('phone',"$username");
+		$this->db->where('password', "$password");
+		$this->db->where('user_type', "student");
+		$this->db->from('tbl_user');
 
-        $query = $this->db->get();
-        return $query->result();
-    }
+		$query = $this->db->get();
+		return $query->result();
+	}
 
-    function find_user($user_id){
-        $this->db->select('*');
-        $this->db->where('user_id',"$user_id");
-        $this->db->from('tbl_user');
+	function find_user($user_id)
+	{
+		$this->db->select('*');
+		$this->db->where('user_id', "$user_id");
+		$this->db->from('tbl_user');
 
-        $query = $this->db->get();
-        return $query->result();
-    }
+		$query = $this->db->get();
+		return $query->result();
+	}
 
-    function change_password($password1 , $user_id)
-    {
-        $this->db->set('password', $password1);
-        $this->db->where('user_id', $user_id);
-        $this->db->update('tbl_user');
-    }
+	function change_password($password1, $user_id)
+	{
+		$this->db->set('password', $password1);
+		$this->db->where('user_id', $user_id);
+		$this->db->update('tbl_user');
+	}
 }
--- teacher/application/models/User_model.php
+++ teacher/application/models/User_model.php
@@ -2,26 +2,30 @@
 
 class User_model extends CI_Model
 {
-    function __construct(){
-        parent::__construct();
-    }
+	function __construct()
+	{
+		parent::__construct();
+	}
 
-    function select_valid_user($username,$password){
+	function select_valid_user($username, $password)
+	{
+		$this->db->select('*');
+		$this->db->where('username', "$username");
+		$this->db->or_where('phone', "$username");
+		$this->db->where('password', "$password");
+		$this->db->where('user_type',"teacher");
+		$this->db->from('tbl_user');
 
-        $this->db->select('*');
-        $this->db->where('username',"$username");
-        $this->db->where('password',"$password");
-        $this->db->from('tbl_user');
+		$query = $this->db->get();
+		return $query->result();
 
-        $query = $this->db->get();
-        return $query->result();
+	}
 
-    }
-    function change_password($password1 , $user_id)
-    {
-        $this->db->set('password', $password1);
-        $this->db->where('user_id', $user_id);
-        $this->db->update('tbl_user');
-    }
+	function change_password($password1, $user_id)
+	{
+		$this->db->set('password', $password1);
+		$this->db->where('user_id', $user_id);
+		$this->db->update('tbl_user');
+	}
 }
 ?>
